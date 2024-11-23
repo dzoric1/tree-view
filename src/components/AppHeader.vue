a55103335eb6070d578b9eca7e1d4a5c991eccf9
dzoric1 <Gerasimov2099@mail.ru>
2024-11-23 14:40:54 +0800

    initial and base settings

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <nav class="header__nav">
+      <RouterLink class="header__nav-link" :to="{ name: 'home' }">
+        Главная
+      </RouterLink>
+      <RouterLink class="header__nav-link" :to="{ name: 'contacts' }">
+        Контакты
+      </RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  @apply bg-gray-100 py-4 shadow-md;
+}
+
+.header__nav {
+  @apply flex items-center justify-center gap-4;
+}
+.header__nav-link {
+  @apply text-blue-500 hover:opacity-50 transition-opacity;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="text-3xl mx-auto">Главная</div>
+</template>
