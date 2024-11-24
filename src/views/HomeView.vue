a02e7a98e5245539cb4c302429f7a06e5114943d
dzoric1 <Gerasimov2099@mail.ru>
2024-11-24 14:12:11 +0800

    add modal to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UiModal from '@/components/UI/UIModal.vue';
+import { ref } from 'vue';
+
+const modalIsOpen = ref<boolean>(false);
+</script>
 
 <template>
-  <div class="text-3xl mx-auto">Главная</div>
+  <div class="text-3xl mx-auto text-center">
+    <button @click="modalIsOpen = !modalIsOpen">Открыть модальное окно</button>
+
+    <teleport to="body">
+      <Transition name="fade">
+        <UiModal
+          v-if="modalIsOpen"
+          title="Диалоговое окно"
+          @close="modalIsOpen = false"
+        />
+      </Transition>
+    </teleport>
+  </div>
 </template>
