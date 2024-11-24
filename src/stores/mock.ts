8bd5bf8d9736e316e9f17f3b06e98b389d6b38d1
dzoric1 <Gerasimov2099@mail.ru>
2024-11-24 21:17:58 +0800

    realized new methods for mock store

--- src/stores/mock.ts
+++ src/stores/mock.ts
@@ -2,6 +2,8 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useMockStore = defineStore('mock', () => {
+  const selectedFileId = ref<number | null>();
+
   const data = ref([
     {
       id: 1,
@@ -11,14 +13,30 @@ export const useMockStore = defineStore('mock', () => {
         {
           id: 3,
           name: 'Папка 1.2',
-          children: [{ id: 4, name: 'Папка 1.2.1', children: [] }],
+          children: [
+            {
+              id: 4,
+              name: 'Папка 1.2.1',
+              children: [{ id: 5, name: 'Папка 1.2.1.1', children: [] }],
+            },
+          ],
         },
       ],
     },
-    { id: 5, name: 'Папка 2', children: [] },
+    { id: 6, name: 'Папка 2', children: [] },
   ]);
 
   const getData = () => data.value;
+  const getSelectedFileId = () => selectedFileId.value;
+  const setSelectedFileId = (id: number | null) => {
+    selectedFileId.value = id;
+  };
 
-  return { data, getData };
+  return {
+    data,
+    selectedFileId,
+    getData,
+    getSelectedFileId,
+    setSelectedFileId,
+  };
 });
