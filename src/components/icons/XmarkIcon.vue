96f7f51e7329c06cf343f1ca9f88b5bae4b85ce2
dzoric1 <Gerasimov2099@mail.ru>
2024-11-24 14:09:51 +0800

    add xmark icon

---
+++ src/components/icons/XmarkIcon.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="icon"></div>
+</template>
+
+<style scoped>
+.icon {
+  position: relative;
+  width: 24px;
+  height: 24px;
+  opacity: 0.2;
+  cursor: pointer;
+  transition: opacity ease 0.5s;
+
+  &:hover {
+    opacity: 1;
+  }
+}
+
+.icon::before,
+.icon::after {
+  content: '';
+  position: absolute;
+  top: 10px;
+  display: block;
+  width: 24px;
+  height: 3px;
+  background: #000;
+}
+
+.icon::before {
+  transform: rotate(45deg);
+}
+
+.icon::after {
+  transform: rotate(-45deg);
+}
+</style>
